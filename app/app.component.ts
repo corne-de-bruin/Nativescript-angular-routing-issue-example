7948ae1201666991f84ad46ef13d504b8e7e8f96
vakrilov <alexander.vakrilov@telerik.com>
2016-08-30 14:24:26 +0300

    unsubscribe form service

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,9 +1,11 @@
 import {Component, OnInit, NgZone, OnDestroy} from "@angular/core";
+import {Router} from "@angular/router";
 import {RouterExtensions} from 'nativescript-angular/router/router-extensions';
 import application = require('application');
 import {BlockScreenService} from './shared/block-screen.service';
 import {APP_ROUTES} from './routes';
 import {NS_ROUTER_DIRECTIVES} from 'nativescript-angular/router';
+import {Subscription} from "rxjs";
 
 @Component({
     selector: "my-app",
@@ -12,16 +14,25 @@ import {NS_ROUTER_DIRECTIVES} from 'nativescript-angular/router';
     directives: [NS_ROUTER_DIRECTIVES]
 })
 export class AppComponent implements OnInit, OnDestroy {
+    private subscription: Subscription;
+    constructor(private zone: NgZone,
+        private blockScreenService: BlockScreenService,
+        private router: RouterExtensions,
+        routerNG: Router) {
 
-    constructor(private zone:NgZone,
-                private blockScreenService: BlockScreenService,
-                private router:RouterExtensions) {
+        routerNG.events.subscribe((e) => {
+            console.log("--EVENT-->: " + e.toString());
+        });
     }
 
+
     ngOnInit(): void {
+        console.log("AppComponent.ngOnInit()");
         this.checkForBlockScreen();
 
         application.on(application.resumeEvent, () => {
+            console.log("----> application.resumeEvent");
+
             this.zone.run(() => {
                 this.blockScreenService.toggleShowBlockScreen();
                 this.checkForBlockScreen();
@@ -31,14 +42,21 @@ export class AppComponent implements OnInit, OnDestroy {
 
     ngOnDestroy(): void {
         application.off(application.resumeEvent);
+        this.subscription.unsubscribe();
     }
 
     private checkForBlockScreen(): void {
-        this.blockScreenService.showBlockScreen
-            .subscribe(res => {
-                if(res === true) {
-                    this.router.navigate([APP_ROUTES.BLOCKSCREEN]);
-                }
-            });
+        console.log("----> checkForBlockScreen");
+        if (!this.subscription) {
+            console.log("----> checkForBlockScreen - subscribing ....");
+            this.subscription = this.blockScreenService.showBlockScreen
+                .subscribe(res => {
+                    if (res === true) {
+                        console.log("navigating to blockscreen");
+
+                        this.router.navigate([APP_ROUTES.BLOCKSCREEN]);
+                    }
+                });
+        }
     }
 }
